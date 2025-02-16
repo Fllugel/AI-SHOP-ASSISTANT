48fe0f8d114b504897511cb3937d33bb021ed85a
Artemidiuss <artemzhykoskuy@gmail.com>
2025-02-16 14:34:41 +0200

    new tool

--- Tools/tools_innit.py
+++ Tools/tools_innit.py
@@ -1,5 +1,6 @@
+from Tools.product_lookup_tool import lookup_product_by_id
 from Tools.shop_info_tool import shop_info_tool
 from Tools.holiday_info_tool import holiday_info_tool
 from Tools.sql_db_tool import find_data_in_db
 
-tools = [find_data_in_db, shop_info_tool, holiday_info_tool ]
+tools = [find_data_in_db, shop_info_tool, holiday_info_tool, lookup_product_by_id]
--- config.py
+++ config.py
@@ -2,4 +2,4 @@
 MAX_MESSAGES_IN_SHORT_TERM_MEMORY = 10
 
 # LLM settings
-OPENAI_MODEL_NAME = 'gpt-4o-mini'
+OPENAI_MODEL_NAME = 'gpt-4o'
